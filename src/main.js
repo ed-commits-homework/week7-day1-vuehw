669a4444fa3131d532f71abe0117cfb83f05a50b
ed-commits <63356087+ed-commits@users.noreply.github.com>
2020-06-22 17:41:57 +0100

    project

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            greeting: "Hello!"
+            greeting: "Hello!",
+            items: [
+                { name: "get started", priority: "high" },
+                { name: "finish everything!", priority: "normal" }
+            ],
+            newItem: "",
+            priority: "normal",
+        },
+        methods: {
+            new_todo: function () {
+                console.log("new todo")
+                this.items.unshift({
+                    name: this.newItem,
+                    priority: this.priority
+                });
+                this.newItem = "";
+                this.priority = "normal";
+            }
         }
-      });
+    });
 });
